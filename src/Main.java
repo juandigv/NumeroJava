b1c846b3e057b3ddd1f16085363567773b833848
JuanDi <juandigv69@gmail.com>
2018-02-01 21:17:35 -0400

    Numeros Pares y Primos

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,23 @@ public class Main {
         String valor = scanner.nextLine();
         int n = Integer.parseInt(valor);
         Numero num = new Numero(n);
-        if (num.esPar())
-            System.out.println("Es Par");
-        else
-            System.out.println("Es Impar");
-        if (num.esPrimo())
-            System.out.println("Es Primo");
-        else
-            System.out.println("No es Primo");
-
-
+        System.out.print("MENU \n 1. Par o Impar \n 2. Primo o no Primo \n Que deseas hacer? :  ");
+        String valorm = scanner.nextLine();
+        int m = Integer.parseInt(valorm);
+        switch(m) {
+            case 1:
+                if (num.esPar())
+                    System.out.println("Es Par");
+                else
+                    System.out.println("Es Impar");
+                break;
+            case 2:
+                if (num.esPrimo())
+                    System.out.println("Es Primo");
+                else
+                    System.out.println("No es Primo");
+                break;
+        }
     }
 }
 
